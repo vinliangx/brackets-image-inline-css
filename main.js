6ba110e02b7a4d6411010438405b793e41e1908c
Vin Rosa <vinliangx@gmail.com>
2014-08-08 12:15:42 -0400

    jslint clear errors

--- main.js
+++ main.js
@@ -1,40 +1,66 @@
+/*global brackets, console, define*/
+
 define(function (require, exports, module) {
     "use strict";
+    var
+        VR_BASE64_IMAGE_COMMAND_ID = "com.vinrosa.base64image.insertimage",
 
-    var REGEX_CSS_URL = /url\(.*?\)/g;
-    var REGEX_CSS_URL_CLEAR = /(url)|([\(\)'"])/g;
-    var REGEX_HTML_IMG = /(<img .*?>+)/gi;
-    var REGEX_HTML_IMG_CLEAR = /(<img .*?((src=)["'])|["'](.*)>+)/g;
-    var REGEX_HTTP = /^(f|ht)tps?:\/\//i;
+        CSS_URL = {
+            selector: new RegExp('url\\(.*?\\)', 'g'),
+            clear: new RegExp('(url)|([\\(\\)\'"])', 'g')
+        },
+        HTML_IMG = {
+            selector: new RegExp('(<img .*?>+)', 'gi'),
+            clear: new RegExp('(<img .*?((src=)["\'])|["\'](.*)>+)', 'g')
+        },
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
+        REGEXS = [CSS_URL, HTML_IMG],
+
+        REGEX_HTTP = /^(f|ht)tps?:\/\//i,
+
         Menus = brackets.getModule("command/Menus"),
+        CommandManager = brackets.getModule("command/CommandManager"),
         AppInit = brackets.getModule("utils/AppInit"),
         FileSystem = brackets.getModule("filesystem/FileSystem"),
         FileUtils = brackets.getModule("file/FileUtils"),
-        EditorManager = brackets.getModule("editor/EditorManager");
+        EditorManager = brackets.getModule("editor/EditorManager"),
+
+
+        contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+
+    function useBase64ForImageUrl(URL, callback) {
+        var canvas = document.createElement("canvas"),
+            img = new Image();
+        img.src = URL;
+        img.onload = function () {
+            canvas.width = this.width;
+            canvas.height = this.height;
+            var ctx = canvas.getContext("2d"),
+                data = null;
+            ctx.drawImage(this, 0, 0);
+            data = canvas.toDataURL("image/jpg");
+            callback(data);
+        };
+    }
 
     function fileToInlineBase64(file, regex) {
         useBase64ForImageUrl(file, function (data) {
-            var editor = EditorManager.getFocusedEditor();
-            if (editor) {
-                var insertionPos = editor.getCursorPos();
-                var line = editor.document.getLine(insertionPos.line);
-                //Reg Ex
-                var matches = line.match(regex.selector);
-                if (matches) {
-                    matches.forEach(function (match) {
-                        var filename = match.replace(regex.clear, '');
-                        line = line.replace(filename, data);
-                        editor.document.replaceRange(line, {
-                            line: insertionPos.line,
-                            ch: 0
-                        }, {
-                            line: insertionPos.line,
-                            ch: line.length
-                        });
+            var editor = EditorManager.getFocusedEditor(),
+                insertionPos = editor.getCursorPos(),
+                line = editor.document.getLine(insertionPos.line),
+                matches = line.match(regex.selector);
+            if (matches) {
+                matches.forEach(function (match) {
+                    var filename = match.replace(regex.clear, '');
+                    line = line.replace(filename, data);
+                    editor.document.replaceRange(line, {
+                        line: insertionPos.line,
+                        ch: 0
+                    }, {
+                        line: insertionPos.line,
+                        ch: line.length
                     });
-                }
+                });
             }
         });
     }
@@ -43,55 +69,33 @@ define(function (require, exports, module) {
         if (REGEX_HTTP.test(image)) {
             return image;
         }
-        var document = EditorManager.getFocusedEditor().document;
-        var path = FileUtils.getDirectoryPath(document.file.fullPath) + image;
+        var document = EditorManager.getFocusedEditor().document,
+            path = FileUtils.getDirectoryPath(document.file.fullPath) + image;
         return path;
     }
 
-    function useBase64ForImageUrl(URL, callback) {
-        var canvas = document.createElement("canvas");
-        var img = new Image();
-        img.src = URL;
-        img.onload = function () {
-            canvas.width = this.width;
-            canvas.height = this.height;
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(this, 0, 0);
-            var data = canvas.toDataURL("image/png");
-            callback(data);
-        }
-    }
 
     function extractImageFile() {
-        var editor = EditorManager.getFocusedEditor();
-        if (editor) {
-            var insertionPos = editor.getCursorPos();
-            var line = editor.document.getLine(insertionPos.line);
-            //Reg Ex
-            var regexs = [{
-                selector: REGEX_CSS_URL,
-                clear: REGEX_CSS_URL_CLEAR
-            }, {
-                selector: REGEX_HTML_IMG,
-                clear: REGEX_HTML_IMG_CLEAR
-            }];
-
-            regexs.some(function (regex) {
-                var matches = line.match(regex.selector);
-                if (matches) {
-                    matches.forEach(function (match) {
-                        var image = match.replace(regex.clear, '');
-                        fileToInlineBase64(resolvePath(image), regex);
-                    });
-                    return true;
-                }
-                return false;
-            });
-        }
+        var editor = EditorManager.getFocusedEditor(),
+            insertionPos = editor.getCursorPos(),
+            line = editor.document.getLine(insertionPos.line);
+        REGEXS.some(function (regex) {
+            console.log(regex);
+            var matches = line.match(regex.selector);
+            console.log(matches);
+            if (matches) {
+                matches.forEach(function (match) {
+                    var image = match.replace(regex.clear, '');
+                    fileToInlineBase64(resolvePath(image), regex);
+                });
+                return true;
+            }
+            return false;
+        });
     }
 
-    var VR_BASE64_IMAGE_COMMAND_ID = "com.vinrosa.base64image.insertimage";
+
     CommandManager.register("Use Data Uri", VR_BASE64_IMAGE_COMMAND_ID, extractImageFile);
-    var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+
     contextMenu.addMenuItem(VR_BASE64_IMAGE_COMMAND_ID);
 });
